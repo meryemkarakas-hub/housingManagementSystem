4cb64bc032a514da1be9acbf7c46eec4c89e7efe
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2024-01-29 15:32:26 +0300

    İlk commit

---
+++ src/main/java/managementSystems/housingManagementSystem/HousingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package managementSystems.housingManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/managementSystems/housingManagementSystem/HousingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package managementSystems.housingManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
