666224274504693ba8616e2740bff04540f2c0a3
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2024-01-31 17:05:57 +0300

    Kayıt ol bitirildi..

--- src/main/java/managementSystems/housingManagementSystem/HousingManagementSystemApplication.java
+++ src/main/java/managementSystems/housingManagementSystem/HousingManagementSystemApplication.java
@@ -2,11 +2,14 @@ package managementSystems.housingManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "managementSystems.housingManagementSystem.application")
 public class HousingManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HousingManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HousingManagementSystemApplication.class, args);
+    }
+
 }
--- src/main/java/managementSystems/housingManagementSystem/application/service/user/impl/UserServiceImpl.java
+++ src/main/java/managementSystems/housingManagementSystem/application/service/user/impl/UserServiceImpl.java
@@ -4,12 +4,24 @@ import lombok.RequiredArgsConstructor;
 import managementSystems.housingManagementSystem.application.core.dto.GeneralMessageDTO;
 import managementSystems.housingManagementSystem.application.core.validator.Validator;
 import managementSystems.housingManagementSystem.application.dto.user.SignUpDTO;
+import managementSystems.housingManagementSystem.application.entity.user.UserRegistration;
+import managementSystems.housingManagementSystem.application.mapper.user.UserMapper;
+import managementSystems.housingManagementSystem.application.repository.user.UserRepository;
 import managementSystems.housingManagementSystem.application.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
 
 
     @Override
@@ -29,7 +41,27 @@ public class UserServiceImpl implements UserService {
         if (!validator.isValid()) {
             return new GeneralMessageDTO(0, validator.getErrorMessage());
         }
+        return checkIfUserExists(signUpDTO);
+    }
 
-        return new GeneralMessageDTO(1, "İşleminiz başarıyla gerçekleşmiştir.");
+    private GeneralMessageDTO checkIfUserExists(SignUpDTO signUpDTO) {
+        List<Supplier<Optional<UserRegistration>>> checks = Arrays.asList(
+                () -> userRepository.findByIdentityNumber(signUpDTO.getIdentityNumber()),
+                () -> userRepository.findByEmailAddress(signUpDTO.getEmailAddress()),
+                () -> userRepository.findByMobileNumber(signUpDTO.getMobileNumber()),
+                () -> userRepository.findByIdentityNumberAndEmailAddressAndMobileNumber(signUpDTO.getIdentityNumber(), signUpDTO.getEmailAddress(), signUpDTO.getMobileNumber()),
+                () -> userRepository.findByIdentityNumberAndEmailAddress(signUpDTO.getIdentityNumber(), signUpDTO.getEmailAddress()),
+                () -> userRepository.findByIdentityNumberAndMobileNumber(signUpDTO.getIdentityNumber(), signUpDTO.getMobileNumber()),
+                () -> userRepository.findByEmailAddressAndMobileNumber(signUpDTO.getEmailAddress(), signUpDTO.getMobileNumber())
+        );
+
+        if (checks.stream().anyMatch(supplier -> supplier.get().isPresent())) {
+            return new GeneralMessageDTO(0, "Kaydolmak istediğiniz bilgiler ile daha önce kayıt işlemi gerçekleşmiştir.Aynı TC kimlik numarası, e-posta adresi ve cep telefonu bilgisi ile kaydolamazsınız.");
+        }
+        UserRegistration userRegistration = userMapper.toEntity(signUpDTO);
+        userRepository.save(userRegistration);
+        return new GeneralMessageDTO(1, "Belirtmiş olduğunuz e-posta adresine aktivasyon linki gönderilmiştir. Linke tıklayarak aktivasyon işlemini gerçekleştirip kayıt işlemini tamamlayınız.");
     }
 }
+
+
