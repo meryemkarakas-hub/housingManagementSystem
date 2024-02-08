757d150c507f7a508f846db9733c970fba6d44fe
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2024-02-08 12:29:29 +0300

    Mail gönderimi için ilgili dosyalar güncellendi.

--- src/main/java/managementSystems/housingManagementSystem/HousingManagementSystemApplication.java
+++ src/main/java/managementSystems/housingManagementSystem/HousingManagementSystemApplication.java
@@ -1,15 +1,12 @@
 package managementSystems.housingManagementSystem;
 
-import managementSystems.housingManagementSystem.application.core.configuration.MailConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @ComponentScan(basePackages = "managementSystems.housingManagementSystem.application")
-@Import(MailConfig.class)
 public class HousingManagementSystemApplication {
 
     public static void main(String[] args) {
