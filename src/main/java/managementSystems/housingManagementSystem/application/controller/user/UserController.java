ddf0b31d88c97bfecd0e130fe3ee51d7383ec677
meryem.karakas <meryemkarakas11@gmail.com>
2024-03-10 17:57:25 +0300

    Yönetim Ekle sayfası için gelen verinin kontrolü yapıldı.

--- src/main/java/managementSystems/housingManagementSystem/application/controller/user/UserController.java
+++ src/main/java/managementSystems/housingManagementSystem/application/controller/user/UserController.java
@@ -2,10 +2,10 @@ package managementSystems.housingManagementSystem.application.controller.user;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import managementSystems.housingManagementSystem.application.core.dto.GeneralMessageDTO;
+import managementSystems.housingManagementSystem.application.dto.management.AddManagementDTO;
 import managementSystems.housingManagementSystem.application.dto.management.SelectManagementDTO;
 import managementSystems.housingManagementSystem.application.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -46,22 +46,12 @@ public class UserController {
         return new ResponseEntity<>(userService.selectManagement(selectManagementDTO), HttpStatus.OK);
     }
 
-    @GetMapping("/checkSession")
-    public String checkSession() {
-        HttpSession session = request.getSession();
-
-        // Session'daki değerleri kontrol etme
-        Long userId = (Long) session.getAttribute("userId");
-        String userRole = (String) session.getAttribute("userRole");
-
-        if (userId != null && userRole != null) {
-            // Session'a kaydedilmiş değerler mevcut
-            // İstediğiniz işlemi yapabilirsiniz
-            return "Session is active!";
-        } else {
-            // Session'a kaydedilmiş değerler yok
-            return "Session is not active!";
-        }
+    @PostMapping("/management-add")
+    ResponseEntity<GeneralMessageDTO> selectManagement(@Valid @RequestBody AddManagementDTO addManagementDTO) {
+        return null;
     }
+
+
+
 }
 
---
+++ src/main/java/managementSystems/housingManagementSystem/application/dto/management/AddManagementDTO.java
@@ -0,0 +1,24 @@
+package managementSystems.housingManagementSystem.application.dto.management;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AddManagementDTO {
+    private String housingTypes;
+    private String city;
+    private String country;
+    private String address;
+    private String apartmentName;
+    private String numberOfFlats;
+    private String siteApartmentName;
+    private String siteSingleHouseName;
+    private String numberOfSingleHouse;
+    private String blockCount;
+    private List<BlocksDTO> blocks;
+}
