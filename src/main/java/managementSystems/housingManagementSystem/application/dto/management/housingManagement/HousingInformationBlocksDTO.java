75a8323123007761999a83859936af557c692a4b
meryem.karakas <meryemkarakas11@gmail.com>
2024-08-18 16:16:29 +0300

    DTO'ya anotasyonlar eklendi.

--- src/main/java/managementSystems/housingManagementSystem/application/dto/management/housingManagement/HousingInformationBlocksDTO.java
+++ src/main/java/managementSystems/housingManagementSystem/application/dto/management/housingManagement/HousingInformationBlocksDTO.java
@@ -1,5 +1,12 @@
 package managementSystems.housingManagementSystem.application.dto.management.housingManagement;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class HousingInformationBlocksDTO {
     private Long id;
     private String blockName;
